8bd9164357fbf8208a58fce6aaaeb31cbad73c37
michaelredruello <redruellomichael@gmail.com>
2025-01-10 22:39:28 +0100

    Added Hamburger Menu

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import GoogleSignin from "/btn_google_signin_dark_pressed_web.png";
 import { auth } from "../../firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
@@ -7,10 +7,50 @@ import "./index.css";
 
 const NavBar = () => {
   const [user] = useAuthState(auth);
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const googleSignIn = async () => {
+    try {
+      const provider = new GoogleAuthProvider();
+      await signInWithRedirect(auth, provider);
+    } catch (error) {
+      console.error("Google sign-in error:", error);
+    }
+  };
+
+  const signOut = () => {
+    auth.signOut();
+  };
+
+  const toggleMenu = () => {
+    setMenuOpen((prevState) => !prevState);
+  };
 
   return (
     <nav className="nav-bar">
       <h1>dotTxT</h1>
+      <div className="hamburger-placeholder" onClick={toggleMenu}>
+        {/* Hamburger placeholder */}
+        &#9776;
+      </div>
+      {menuOpen && (
+        <div className="menu">
+          {user ? (
+            <button onClick={signOut} className="sign-out" type="button">
+              Sign Out
+            </button>
+          ) : (
+            <>
+              <button onClick={googleSignIn} className="sign-in" type="button">
+                <img src={GoogleSignin} alt="Sign in with Google" />
+              </button>
+              <button className="sign-out" type="button">
+                Profile
+              </button>
+            </>
+          )}
+        </div>
+      )}
     </nav>
   );
 };
