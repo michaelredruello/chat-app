3f29ac6e1f89bc33c75e459edfcfc71bcb7d43a0
michaelredruello <redruellomichael@gmail.com>
2025-01-14 06:26:20 +0100

    messages component restyling

--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -6,19 +6,22 @@ import "./index.css";
 const Message = ({ message }) => {
   const [user] = useAuthState(auth);
 
+  const isCurrentUser = user && message.uid === user.uid;
+
   return (
-    <div
-      className={`chat-bubble ${
-        user && message.uid === user.uid ? "right" : ""
-      }`}
-    >
+    <div className={`chat-bubble ${isCurrentUser ? "right" : ""}`}>
       <img
         className="chat-bubble__left"
         src={message.avatar}
-        alt="user avatar"
+        alt={`${message.name}'s avatar`}
       />
       <div className="chat-bubble__right">
-        <p className="user-name">{message.name}</p>
+        <p
+          className="user-name"
+          style={{ color: isCurrentUser ? "#204d31" : "#ecfde5" }}
+        >
+          {message.name}
+        </p>
         <p className="user-message">{message.text}</p>
       </div>
     </div>
