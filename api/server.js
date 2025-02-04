88ef60d211dadf381ab4a054179b7e7c66e21400
michaelredruello <redruellomichael@gmail.com>
2025-02-04 05:38:19 +0100

    server fix

--- api/server.js
+++ api/server.js
@@ -16,7 +16,9 @@ admin.initializeApp({
   credential: admin.credential.cert({
     project_id: process.env.FIREBASE_PROJECT_ID,
     client_email: process.env.FIREBASE_CLIENT_EMAIL,
-    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    private_key: process.env.FIREBASE_PRIVATE_KEY.split(String.raw`\n`).join(
+      "\n"
+    ),
   }),
 });
 
